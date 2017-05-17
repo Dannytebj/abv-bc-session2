adc87414b8324e9a399d5c3955792a72dce8c52c
Dannyb <dannyb4me@cbn.gov>
2017-05-17 09:51:04 +0100

    Finish setting up project dependencies

---
+++ app.js
@@ -0,0 +1,16 @@
+module.exports ={ 
+	
+fizzBuzz= function(number){
+	 
+	  if ((number % 3 === 0) && (number % 5 !== 0) ){
+	    return 'Fizz';
+	  }else if (number % 5 === 0 && (number % 3 !== 0)){
+	    return 'Buzz';
+	  }else if ((number % 3 >0) && (number % 5 > 0)){
+	    return number;
+	  }else{
+	    return 'FizzBuzz';
+	  }
+
+	}
+}
