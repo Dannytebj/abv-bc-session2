9a1f3b9c2e6154234a3688648c94c8ac734eb658
Dannyb <dannyb4me@cbn.gov>
2017-05-17 16:49:28 +0100

    updates spec and app file

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
-module.exports ={
-class Car {
+export class Car {
 
   constructor(name,model,type){
     
     this.name = (name ==='') ? "General":name; 
     this.model =(model ==='') ?"GM":model;
     this.numOfDoor =(this.name ==='Porshe'|| this.name === 'Koenigsegg') ? 2:4;
-    this.type=type;
+    this.type = type;
     this.numOfWheels = (this.type ==='trailer') ? 8:4; 
     this.isSaloon =(this.type !=='trailer') ? true:false;
     
-    this.speed = this.prototype.speed= function() {
-      return this.speed = (this.drive ==='') ? "0 km/h":speed;
-    }
+  
   }
   
 drive(speed){
-  if(this.name ==='Porshe'){
+  if(this.name ==='Porshe' && speed != 0){
       this.speed = '250 km/h';
-    }else if(this.name === 'Man'){
+    }else if(this.name === 'Man' && speed !=0){
       this.speed = '77 km/h';
+    }else{
+      this.speed= '0 km/h';
     }
   }
 
-  }  
+  
 
 }
--- spec/spec.js
+++ spec/spec.js
@@ -1,58 +1,58 @@
-const myApp = require('../app.js');
-describe("Car Class: Create a car, make it drive", function() {
+import {Car} from '../app.js';
+ describe("Car Class: Create a car, make it drive", function() {
 
     it("The car should be a type of `object`, and an instance of the `Car` class", function() {
       var honda = new Car('Honda');
-      myApp.expect(typeof honda).toEqual(typeof {});
-      myApp.expect(honda instanceof Car).toBeTruthy();
+      expect(typeof honda).toEqual(typeof {});
+      expect(honda instanceof Car).toBeTruthy();
     });
 
     it("The car should be called 'General' if no name is passed as a parameter", function() {
       var gm = new Car();
-      myApp.expect(gm.name).toEqual('General');
-      myApp.expect(gm.model).toBe('GM');
+      expect(gm.name).toEqual('General');
+      expect(gm.model).toBe('GM');
     });
 
     it("The car name and model should be a property of the car", function() {
       var toyota  = new Car('Toyota', 'Corolla');
-      myApp.expect(toyota.name).toBe('Toyota');
-      myApp.expect(toyota.model).toBe('Corolla');
+      expect(toyota.name).toBe('Toyota');
+      expect(toyota.model).toBe('Corolla');
     });
 
     it("The car shoud have four (4) doors except its a Porshe or Koenigsegg", function() {
       var opel  = new Car('Opel', 'Omega 3');
-      myApp.expect(opel.numOfDoors).toBe(4);
+      expect(opel.numOfDoors).toBe(4);
 
       var porshe = new Car('Porshe', '911 Turbo');
-      myApp.expect(porshe.numOfDoors).toBe(2);
+      expect(porshe.numOfDoors).toBe(2);
       porshe.drive(5);
-      myApp.expect(porshe.speed).toBe('250 km/h');
-      myApp.expect((function(){return new Car('Koenigsegg', 'Agera R');}()).numOfDoors).toBe(2);
+      expect(porshe.speed).toBe('250 km/h');
+      expect((function(){return new Car('Koenigsegg', 'Agera R');}()).numOfDoors).toBe(2);
     });
 
     it("The car shoud have four (4) wheels except its a type of trailer", function() {
       var man  = new Car('MAN', 'Truck', 'trailer');
-      myApp.expect(man.numOfWheels).toBe(8);
-      myApp.expect(man.isSaloon).toBe(false);
+      expect(man.numOfWheels).toBe(8);
+      expect(man.isSaloon).toBe(false);
 
       var koenigsegg = new Car('Koenigsegg', 'Agera R');
-      myApp.expect(koenigsegg.numOfWheels).toBe(4);
-      myApp.expect(koenigsegg.isSaloon).toBeTruthy();
+      expect(koenigsegg.numOfWheels).toBe(4);
+      expect(koenigsegg.isSaloon).toBeTruthy();
     });
 
     it("The Trailer should have speed 0 km/h until you put `the pedal to the metal`", function() {
       var man  = new Car('MAN', 'Truck', 'trailer');
-      myApp.expect(man.speed).toBe('0 km/h');
+      expect(man.speed).toBe('0 km/h');
       man.drive(7);
-      myApp.expect(man.speed).toBe('77 km/h');
+      expect(man.speed).toBe('77 km/h');
     });
 
     it("The car drive function should return the instance of the Car class", function() {
       var man  = new Car('MAN', 'Truck', 'trailer');
       var drivingMan = man.drive(7);
-      myApp.expect(drivingMan instanceof Car).toBeTruthy();
-      myApp.expect(typeof drivingMan.drive).toBe(typeof (function (){}));
-      myApp.expect(man.speed).toBe(drivingMan.speed);
+      expect(drivingMan instanceof Car).toBeTruthy();
+      expect(typeof drivingMan.drive).toBe(typeof (function (){}));
+      expect(man.speed).toBe(drivingMan.speed);
     });
 
   });
